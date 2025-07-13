267464cb21a628f219a38381f57c6502c40c7947
SoFa325 <sofyabel@inbox.ru>
2025-07-13 14:01:58 +0300

    Initial commit

---
+++ src/main/java/org/example/dz1_kafka/Dz1KafkaApplication.java
@@ -0,0 +1,13 @@
+package org.example.dz1_kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dz1KafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Dz1KafkaApplication.class, args);
+    }
+
+}
